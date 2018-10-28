fc37facb7aec8e911f161f889e25681d1a1b7176
lucianavarela <lucianavarela@hotmail.es>
2018-10-28 17:44:08 -0300

    cambios;

--- index.php
+++ index.php
@@ -81,4 +81,21 @@ $app->group('/api', function () use ($app) {
   });
 })->add(\MWparaAutentificar::class . ':VerificarUsuario');
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+  return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+  $response = $next($req, $res);
+  return $response
+          ->withHeader('Access-Control-Allow-Origin', '*')
+          ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+          ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+  $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+  return $handler($req, $res);
+});
+
 $app->run();
