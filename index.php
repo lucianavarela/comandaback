05d04f795e2e0747ec759021932dbdb749e79005
lucianavarela <lucianavarela@hotmail.es>
2018-11-11 22:27:23 -0300

    cambiosn

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $app->post('/login/', \login::class . ':UserLogin');
 $app->group('/api', function () use ($app) {
   $this->group('/comanda', function () use ($app) {
     $this->get('/', \comandaApi::class . ':TraerTodos');
-    $this->get('/{codigoMesa}/{codigoComanda}', \comandaApi::class . ':TraerUno');
+    $this->get('/cliente/{codigoMesa}/{codigoComanda}', \comandaApi::class . ':TraerUno');
     $this->post('/', \comandaApi::class . ':CargarUno')->add(\MWparaAutentificar::class . ':VerificarMozo')->add(\MWparaAutentificar::class . ':VerificarToken');
     $this->post('/foto/{codigo}', \comandaApi::class . ':CargarFoto')->add(\MWparaAutentificar::class . ':VerificarMozo')->add(\MWparaAutentificar::class . ':VerificarToken');
     $this->post('/cobrar', \comandaApi::class . ':CobrarUno')->add(\MWparaAutentificar::class . ':VerificarMozo')->add(\MWparaAutentificar::class . ':VerificarToken');
